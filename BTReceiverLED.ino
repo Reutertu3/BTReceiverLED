9e70dc6726ea78e313ed29b12f9b1744a5907840
Reutertu3 <51541166+Reutertu3@users.noreply.github.com>
2023-08-28 02:45:47 +0200

    Update BTReceiverLED.ino

--- BTReceiverLED.ino
+++ BTReceiverLED.ino
@@ -1,30 +1,59 @@
+/*
+ * BT Receiver
+ * 
+ * LCK 15
+ * BCK 4
+ * DATA 2
+*/
 #include <driver/rtc_io.h>                      
 #include "BluetoothA2DPSink.h"
+#define AUTOCONNECT_TRY_NUM 2
 
 BluetoothA2DPSink a2dp_sink;
 
 RTC_DATA_ATTR int bootCounter = 0;              //Persistent boot counter
-const int freq = 5000;
+const int freq = 20000;
 const int ledChannelRed = 0;
 const int ledChannelGreen = 1;
 const int ledChannelBlue = 2;
-const int resolution = 8;
+const int resolution = 11;
 
-const int ledPinGreen = 18;
-const int ledPinBlue = 23;
-const int ledPinRed = 19;
+const int ledPinGreen = 33;
+const int ledPinBlue = 32;
+const int ledPinRed = 25;
 //const int ledInternal = 2;
-const int PIN_BUTTON = 27;  
+const int PIN_BUTTON = 13;  
 int button_state = 0;
 int ledState = LOW;
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
-const long interval = 200;                          //Interval for pulse length
+const long interval = 500;                          //Interval for pulse length
 // -------------------------------------------------------------------------
 // Setup
 // -------------------------------------------------------------------------
 void setup() {
+  i2s_pin_config_t my_pin_config = {
+        .bck_io_num = 4,
+        .ws_io_num = 15,
+        .data_out_num = 2,
+        .data_in_num = I2S_PIN_NO_CHANGE
+    };
+  a2dp_sink.set_pin_config(my_pin_config);
 
+  static i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
+      .sample_rate = 44100, // updated automatically by A2DP
+      .bits_per_sample = (i2s_bits_per_sample_t)32,
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_STAND_I2S),
+      .intr_alloc_flags = 0, // default interrupt priority
+      .dma_buf_count = 8,
+      .dma_buf_len = 64,
+      .use_apll = true,
+      .tx_desc_auto_clear = true // avoiding noise in case of data unavailability
+  };
+  a2dp_sink.set_i2s_config(i2s_config);
+    
 ledcSetup(ledChannelRed, freq, resolution);
 ledcSetup(ledChannelGreen, freq, resolution);
 ledcSetup(ledChannelBlue, freq, resolution);
@@ -33,19 +62,19 @@ ledcAttachPin(ledPinGreen, ledChannelGreen);
 ledcAttachPin(ledPinBlue, ledChannelBlue);
 
 //pinMode(ledInternal, OUTPUT);  
-pinMode (ledPinGreen, OUTPUT);
-pinMode (ledPinBlue, OUTPUT);
-pinMode (ledPinRed, OUTPUT);
+//pinMode (ledPinGreen, OUTPUT);
+//pinMode (ledPinBlue, OUTPUT);
+//pinMode (ledPinRed, OUTPUT);
 pinMode(PIN_BUTTON, INPUT_PULLDOWN);
-digitalWrite (ledPinRed, HIGH);
-rtc_gpio_pulldown_en((gpio_num_t)GPIO_NUM_27);                                            //Enables ESP32 pullup or pulldown resistor during deep sleep
-esp_sleep_enable_ext0_wakeup((gpio_num_t)GPIO_NUM_27, RISING);
-firstBoot();                                                                                                                    //If first time booting, go directly into sleep (prevents full startup after providing power)
-cycle (ledChannelRed, 0, 200);
-cycle (ledChannelBlue, 0, 200);
-cycle (ledChannelGreen, 0, 200);
+//digitalWrite (ledPinRed, HIGH);
+rtc_gpio_pulldown_en((gpio_num_t)GPIO_NUM_13);                                            //Enables ESP32 pullup or pulldown resistor during deep sleep
+esp_sleep_enable_ext0_wakeup((gpio_num_t)GPIO_NUM_13, RISING);
+firstBoot();                                                                             //If first time booting, go directly into sleep (prevents full startup after providing power)
+cycle (ledChannelRed, 0, 200, 1);
+cycle (ledChannelBlue, 0, 200, 1);
+cycle (ledChannelGreen, 0, 200, 1);
 delay(50);
-a2dp_sink.start("BluetoothESP32");
+a2dp_sink.start("BTReceiverV1");
 }
 
 void loop() 
@@ -57,9 +86,9 @@ button_state = digitalRead(PIN_BUTTON);
 if (button_state == HIGH) {
     ledcWrite(ledChannelGreen, 0);
     ledcWrite(ledChannelBlue, 0);
-    ledcWrite(ledChannelRed, 230); 
-    delay(200);    
-    for(int dutyCycle = 230; dutyCycle >= 0; dutyCycle--){
+    ledcWrite(ledChannelRed, 150); 
+    delay(500);    
+    for(int dutyCycle = 200; dutyCycle >= 0; dutyCycle--){
     ledcWrite(ledChannelRed, dutyCycle);
     delay (3);
     }
@@ -67,21 +96,20 @@ if (button_state == HIGH) {
   }
 
 switch (a2dp_sink.get_connection_state()) {
-        case ESP_A2D_CONNECTION_STATE_CONNECTED:
+        case ESP_A2D_CONNECTION_STATE_CONNECTED: //Verbunden - Durchgehend blau
             ledcWrite(ledChannelRed, 0);
             ledcWrite(ledChannelGreen, 0);
             ledcWrite(ledChannelBlue, 150);           
             break;
-        case ESP_A2D_CONNECTION_STATE_CONNECTING:
+        case ESP_A2D_CONNECTION_STATE_CONNECTING: //Verbindungsaufbau - Blau blinkend
             ledcWrite(ledChannelRed, 0);
             ledcWrite(ledChannelGreen, 0);
-            //digitalWrite (ledPinBlue, LOW);
-            PWMblink(ledChannelBlue, 0, 150);
+            cycle (ledChannelBlue, 20, 200, 3);
             break;
-        case ESP_A2D_CONNECTION_STATE_DISCONNECTED:
+        case ESP_A2D_CONNECTION_STATE_DISCONNECTED: //Bereit für Verbundung - Grün blinkend
             ledcWrite(ledChannelRed, 0);
             ledcWrite(ledChannelBlue, 0);
-            PWMblink(ledChannelGreen, 0, 150);
+            cycle (ledChannelGreen, 20, 200, 3);
             break;
 
       }
@@ -99,17 +127,16 @@ void firstBoot()
 }
 
 
-void cycle (int ledChannel, int minDutyCycle, int maxDutyCycle)
+void cycle (int ledChannel, int minDutyCycle, int maxDutyCycle, int blinkDelay)
 {
     for(int dutyCycle = minDutyCycle; dutyCycle <= maxDutyCycle; dutyCycle++){ 
     ledcWrite(ledChannel, dutyCycle);
-    delay (1);
+    delay (blinkDelay);
     }
     for(int dutyCycle = maxDutyCycle; dutyCycle >= minDutyCycle; dutyCycle--){
     ledcWrite(ledChannel, dutyCycle);
-    delay (1);
+    delay (blinkDelay);
     }
-    ledcWrite(ledChannel,0);
 }
 
 
